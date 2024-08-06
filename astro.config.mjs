9acb08a3565da6f1c1b0aa5a9f74c6bd31120f87
mferrero172 <mferrero172@gmail.com>
2024-08-06 14:54:35 +0000

    Analitics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'static',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
+  output: "server",
+  adapter: vercel()
 });
